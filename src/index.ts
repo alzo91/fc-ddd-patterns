9616a7a6d967c8c95aa4e86a8050cd7c46faaf59
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2024-03-13 20:50:07 -0300

    moving folders (entities, services) to domain

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Address } from "./entities/address";
-import { Customer } from "./entities/customer";
-import { Order } from "./entities/order";
-import { OrderItem } from "./entities/order_items";
+import { Address } from "./domain/entities/address";
+import { Customer } from "./domain/entities/customer";
+import { Order } from "./domain/entities/order";
+import { OrderItem } from "./domain/entities/order_items";
 
 let customer = new Customer({ name: "Lavínia Zotarelli de Oliveira" });
 console.log(JSON.stringify({ customer }));
